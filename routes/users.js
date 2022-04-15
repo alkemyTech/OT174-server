1e0e68dfb8fe94c65b68e0665f9fee47bff4587b
yego0210 <diegotrujillo0210@gmail.com>
2022-04-15 11:21:29 -0500

    validator password

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const { validarCampos} = require('../middlewares/validar-campos');
 var user = require('../models/index')
 const UsuarioController = require('../controllers/users')
 const { emailExiste, existeUsuarioPorId } = require('../helpers/db-validatiors')
+const bcryptjs = require('bcryptjs')
+
 
 var router = express.Router()
 
@@ -16,6 +18,14 @@ router.post('/auth/register',[
   check('firstName', 'Name is required').not().isEmpty(),
   check('lastName', 'Last name is required').not().isEmpty(),
   check('password', 'The password must have at least 6 characters').isLength({ min: 6 }),
+  check('password', 'The password must have at least one number').matches(/\d/),
+  check('password', 'The password must have at least one uppercase letter').matches(/[A-Z]/),
+  check('password', 'Password must have at least one lowercase letter').matches(/[a-z]/),
+  check('password', 'The password must have at least one special character').matches(/[!@#$%^&*(),.?":{}|<>]/),
+
+ 
+
+  
   check('email', 'invalid email').isEmail(),
   check('email').custom(emailExiste),
  
