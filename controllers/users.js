85a26652472e4fee5ccf32e92a5f32e2310387c0
yego0210 <diegotrujillo0210@gmail.com>
2022-04-15 10:02:32 -0500

    Create user

--- controllers/users.js
+++ controllers/users.js
@@ -3,47 +3,49 @@ var user = require("../models/index");
 const AddUsuario = (req, res, next) => {
   const { firstName, lastName, email, password, image, roleId } = req.body;
 
-  if (!firstName || !lastName || !email || !password || !image || !roleId) {
-    return res.status(400).json({
-      success: false,
-      message: "All fields are required to register (¬‿¬)",
-    });
-  }
+  //   if (!firstName || !lastName || !email || !password || !image || !roleId) {
+  //     return res.status(400).json({
+  //       success: false,
+  //       message: "All fields are required to register (¬‿¬)",
+  //     });
+  //   }
   usersdb = user.User;
-  usersdb.findAll({ where: { email: email } }).then((user) => {
-    if (user.length > 0) {
-      res.status(400).json({
-        msg: `User ${email} already exists`,
+  usersdb
+    .create({
+      firstName,
+      lastName,
+      email,
+      password,
+      image,
+      roleId,
+      // body
+    })
+    .then(function (user) {
+      res.json({
+        success: true,
+        message: "Usuario creado correctamente",
+        user: user,
       });
-    } else {
-      usersdb
-        .create({
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          password: password,
-          image: image,
-          roleId: roleId,
-        })
-        .then((user) => {
-          res.status(200).json({
-            state: true,
-            message: "User created successfully",
-            msg: `Felicitaciones ${firstName} ${lastName} ya puedes iniciar sesion`,
-            user: user,
-          });
-        })
-        .catch((err) => {
-          res.status(400).json({
-            state: false,
-            message: "Error creating user",
-            error: err,
-          });
-        });
-    }
-  });
+    })
+    .catch(function (err) {
+      res.json({
+        success: false,
+        message: "Error al crear el usuario",
+        error: err,
+      });
+    });
 };
 
 module.exports = {
-    AddUsuario,
+  AddUsuario,
 };
+
+// crear el usuario en la base de datos
+// const sql = `INSERT INTO users (firstName, lastName, email, password, image, roleId) VALUES ('${firstName}', '${lastName}', '${email}', '${password}', '${image}', '${roleId}')`;
+// user.User.sequelize.query(sql).then((user) => {
+//   res.status(200).json({
+//     message: "Usuario creado correctamente",
+
+//   });
+// }
+// );
