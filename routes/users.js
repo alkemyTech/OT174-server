f7b285a89b15142cdb2545855cb2a643b5529340
yego0210 <diegotrujillo0210@gmail.com>
2022-04-14 20:23:40 -0500

    post -- save --err

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,79 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var user = require("../models/index");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource GET");
+});
+
+router.post("/auth/register", function (req, res, next) {
+  const { firstName, lastName, email, password, image, roleId } = req.body;
+ // verificar que el email no exista en la base de datos 
+
+  // buscar que el correo exista en la base de datos y que no este registrado
+  user.User.findOne({ where: { email: email } }).then((user) => {
+    if (user) {
+      res.status(400).json({
+        message: "El correo ya esta registrado",
+      });
+    } 
+ 
+
+  user.User.create({
+    firstName,
+    lastName,
+    email,
+    password,
+    image,
+    roleId,
+    // body
+  })
+    .then(function (user) {
+      res.json({
+        success: true,
+        message: "Usuario creado correctamente",
+        user: user,
+      });
+    })
+    .catch(function (err) {
+      res.json({
+        success: false,
+        message: "Error al crear el usuario",
+        error: err,
+      });
+    });
+
+
+
+
+  });
 });
 
 module.exports = router;
+
+
+
+// user.User.create({
+//   firstName,
+//   lastName,
+//   email,
+//   password,
+//   image,
+//   roleId,
+//   // body
+// })
+//   .then(function (user) {
+//     res.json({
+//       success: true,
+//       message: "Usuario creado correctamente",
+//       user: user,
+//     });
+//   })
+//   .catch(function (err) {
+//     res.json({
+//       success: false,
+//       message: "Error al crear el usuario",
+//       error: err,
+//     });
+//   });
