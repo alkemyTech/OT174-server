294bccb78d337ff27aac6c2bd110676746968b0e
yego0210 <diegotrujillo0210@gmail.com>
2022-04-15 00:42:31 -0500

    Database registration ready

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,27 @@
-var express = require("express");
-var router = express.Router();
-var user = require("../models/index");
+var express = require('express')
+var router = express.Router()
+var user = require('../models/index')
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource GET");
-});
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource GET')
+})
 
-router.post("/auth/register", function (req, res, next) {
-
-  const { firstName, lastName, email, password, image, roleId } = req.body;
+router.post('/auth/register', function (req, res, next) {
+  const { firstName, lastName, email, password, image, roleId } = req.body
 
   if (!firstName || !lastName || !email || !password || !image || !roleId) {
     return res.status(400).json({
       success: false,
-      message: "All fields are required to register (¬‿¬)",
-    });
+      message: 'All fields are required to register (¬‿¬)'
+    })
   }
-  usersdb = user.User;
-  usersdb.findAll({ where: { email: email } }).then((user) => {
+  usersdb = user.User
+  usersdb.findAll({ where: { email: email } }).then(user => {
     if (user.length > 0) {
       res.status(400).json({
-        msg: `User ${email} already exists`,  
-      }); 
+        msg: `User ${email} already exists`
+      })
     } else {
       usersdb
         .create({
@@ -31,31 +30,30 @@ router.post("/auth/register", function (req, res, next) {
           email: email,
           password: password,
           image: image,
-          roleId: roleId,
+          roleId: roleId
         })
-        .then((user) => {
-          
+        .then(user => {
           res.status(200).json({
             state: true,
-            message: "User created successfully",
+            message: 'User created successfully',
             msg: `Felicitaciones ${firstName} ${lastName} ya puedes iniciar sesion`,
-            user: user,
-          });
-        }) 
-        .catch((err) => {
+            user: user
+          })
+        })
+        .catch(err => {
           res.status(400).json({
             state: false,
-            message: "Error creating user",
-            error: err,
-          });
-        });
+            message: 'Error creating user',
+            error: err
+          })
+        })
     }
-  });
-});
+  })
+})
 
-console.log();
+console.log()
 
-module.exports = router;
+module.exports = router
 
 // crear el usuario en la base de datos
 // const sql = `INSERT INTO users (firstName, lastName, email, password, image, roleId) VALUES ('${firstName}', '${lastName}', '${email}', '${password}', '${image}', '${roleId}')`;
