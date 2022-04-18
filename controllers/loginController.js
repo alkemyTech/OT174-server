0f697bf93958263ab2e050e1cc7520a013bed3dc
gonzacs96 <gonzalosalinasss.96@gmail.com>
2022-04-18 15:01:14 -0300

    create login controller

---
+++ controllers/loginController.js
@@ -0,0 +1,25 @@
+const { User } = require("../models/index");
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
+
+const loginController = async (req, res, next) => {
+  const { email, password } = req.body;
+  const errors = validationResult(req);
+  //verifica si hay errores en la validacion (email,password)
+  if (!errors.isEmpty()) {
+    return res.json({ok:false});
+  }
+  try {
+    //busca el usuario con ese email en la db
+    const user = await User.findOne({ where: { email } });
+    //verifica si el usuario existe y compara las passwords
+    if (user && bcrypt.compareSync(password, user.password)) {
+      return res.json({ user: user.email });
+    }
+    res.json({ ok: false });
+  } catch (error) {
+    res.json({ ok: false });
+  }
+};
+
+module.exports = loginController;
