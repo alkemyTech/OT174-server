ffb5130ee29e8a55b8a56a0ff342daa0f99369a8
javicara <javicaraballo1@gmail.com>
2022-04-19 10:02:09 -0300

    copy model and migration

---
+++ models/organization.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const alias = "Organization";
+  const cols = {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    welcomeText: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    aboutUsText: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    }
+  }
+  const config = {
+    tableName: "organization",
+    timestamps: true,
+    softDelete: true
+  }
+  const Organization = sequelize.define(alias, cols, config);
+  return Organization;
+};
