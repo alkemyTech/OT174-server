02071bcf10befadad6a1f228586e96c936eb5ccc
faugconti <faugconti@gmail.com>
2022-04-19 13:31:40 -0300

    created categories base controller

---
+++ controllers/categoryController.js
@@ -0,0 +1,7 @@
+// const { Category } = require("../models");
+
+exports.getAllCategories = async (req, res, next) => {};
+exports.getCategoryById = async (req, res, next) => {};
+exports.createCategory = async (req, res, next) => {};
+exports.deleteCategoryById = async (req, res, next) => {};
+exports.updateCategory = async (req, res, next) => {};
