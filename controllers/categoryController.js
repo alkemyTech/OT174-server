2f710c2fb9bc84fef092907089ba7d844c719fb1
faugconti <faugconti@gmail.com>
2022-04-20 22:18:18 -0300

    fix routes names

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,4 +4,4 @@ exports.getAllCategories = async (req, res, next) => {};
 exports.getCategoryById = async (req, res, next) => {};
 exports.createCategory = async (req, res, next) => {};
 exports.deleteCategoryById = async (req, res, next) => {};
-exports.updateCategory = async (req, res, next) => {};
+exports.updateCategoryById = async (req, res, next) => {};
