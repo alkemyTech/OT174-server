e995ab3d81e642fa37a25c4752484bc345410d2a
javicara <javicaraballo1@gmail.com>
2022-04-21 11:55:17 -0300

    deleting log and changing verify to validate

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,13 +1,12 @@
 const jwt = require("jsonwebtoken");
 
 
-async function verifyToken(req, res, next) {
+async function validateToken(req, res, next) {
     try {
       const token = req.headers["x-access-token"];
       if (token) {
-        const verify = await jwt.verify(token, process.env.SECRET_JWT);
-        req.userId = verify.id;
-        console.log(verify);
+        const validate = await jwt.verify(token, process.env.SECRET_JWT);
+        req.userId = validate.id;
       } else {
         return res.status(401).json({
           auth: false,
