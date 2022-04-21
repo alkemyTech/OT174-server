a05a1d53642807136d148ede818682390c708593
Paolo Belforte <75280184+belfortep@users.noreply.github.com>
2022-04-20 23:03:14 -0300

    update slide migration with relations

--- migrations/20220415024554-create-slide.js
+++ migrations/20220415024554-create-slide.js
@@ -1,45 +1,59 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Slides', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      organizationId: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-      },
-      text: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      imageUrl: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-      },
-      order:{
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        unique: true
-      },
-      deletedAt: {
-        type: Sequelize.DATE,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Slides');
-  }
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('Slides', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            text: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            imageUrl: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true
+            },
+            order: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                unique: true
+            },
+            deletedAt: {
+                type: Sequelize.DATE,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+
+        await queryInterface.addColumn(
+            'Slide',
+            'organizationId',
+            {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'Organization',
+                    key: 'id'
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'SET NULL'
+            }
+        );
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('Slides');
+        await queryInterface.removeColumn(
+            'Slides',
+            'organizationId'
+        )
+    }
 };
