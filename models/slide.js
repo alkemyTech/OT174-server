d55232857aa7a851ec130be11a79c55ccacc6b55
Paolo Belforte <75280184+belfortep@users.noreply.github.com>
2022-04-20 23:01:19 -0300

    Slide.js association with Organization model

--- models/slide.js
+++ models/slide.js
@@ -1,27 +1,28 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Slide extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Slide.init({
-    organizationId: DataTypes.INTEGER,
-    text: DataTypes.STRING,
-    imageUrl: DataTypes.STRING,
-    order: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Slide',
-    paranoid: true
-  });
-  return Slide;
+    class Slide extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            this.belongsTo(models.Organization);
+        }
+    };
+    Slide.init({
+        organizationId: DataTypes.INTEGER,
+        text: DataTypes.STRING,
+        imageUrl: DataTypes.STRING,
+        order: DataTypes.INTEGER
+    }, {
+        sequelize,
+        modelName: 'Slide',
+        paranoid: true
+    });
+    return Slide;
 };
