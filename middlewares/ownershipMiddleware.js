57a9f879d5174e05d74249b5a213bf75f7c86bfa
faugconti <faugconti@gmail.com>
2022-04-21 18:27:07 -0300

    fix changed request property name

--- middlewares/ownershipMiddleware.js
+++ middlewares/ownershipMiddleware.js
@@ -2,6 +2,6 @@ const { isAdmin } = require("./adminMiddleware");
 
 module.exports = {
   checkOwnership: (req, res, next) => {
-    return +req.params.id === req.decodedToken.userId ? next() : isAdmin();
+    return +req.params.id === req.userId ? next() : isAdmin();
   },
 };
