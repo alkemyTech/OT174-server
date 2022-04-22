b99c54261fe63a85d3b20ba77584f7730356fee2
javicara <javicaraballo1@gmail.com>
2022-04-22 11:55:10 -0300

    improving responsability of the method validateToken

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,24 +1,19 @@
-const jwt = require("jsonwebtoken");
+const {RESPONSE_OK} = require('../enums/messages');
 
+const {
+  validateExistsToken,
+  getUserIdByToken,
+} = require("../services/tokenService");
 
-async function validateToken(req, res, next) {
-    try {
-      const token = req.headers["x-access-token"];
-      if (token) {
-        const validate = await jwt.verify(token, process.env.SECRET_JWT);
-        req.userId = validate.id;
-      } else {
-        return res.status(401).json({
-          auth: false,
-          msg: "No token provided",
-        });
-      }
-      next()
-    } catch {
-      res.status(403).json({
-        auth: false,
-        msg: "Invalid token provided",
-      });
-    }
-   
-  }
+async function validateToken(req, res) {
+  const token = validateExistsToken(req, res);
+  const id = getUserIdByToken(token);
+  res.status(200).json({
+    auth: RESPONSE_OK,
+    id: id,
+  });
+}
+
+module.exports = {  
+  validateToken,
+};
