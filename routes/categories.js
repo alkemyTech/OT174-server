b4281d9d00c6d3b64628aaa25158f188998b516c
faugconti <faugconti@gmail.com>
2022-04-25 12:34:07 -0300

    fix added validation in routes/categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
 const categoriesController = require("../controllers/categoriesController");
 const { isAdmin } = require("../middlewares/adminMiddleware");
 const { validateToken } = require("../middlewares/authMiddleware");
+
+const validateCreate = [check("name").notEmpty().isString()];
 
 router.get("", validateToken, isAdmin, categoriesController.getAllCategories);
 router.get(
@@ -11,7 +14,13 @@ router.get(
   isAdmin,
   categoriesController.getCategoryById
 );
-router.post("", validateToken, isAdmin, categoriesController.createCategory);
+router.post(
+  "",
+  validateToken,
+  isAdmin,
+  validateCreate,
+  categoriesController.createCategory
+);
 router.put(
   "/:id",
   validateToken,
