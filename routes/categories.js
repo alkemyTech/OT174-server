4e9eed2fe9bd1b1268de563516b4b016c8744b67
faugconti <faugconti@gmail.com>
2022-04-25 12:10:20 -0300

    fix routes/categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const categoriesController = require("../controllers/categoriesController");
+const { isAdmin } = require("../middlewares/adminMiddleware");
+const { validateToken } = require("../middlewares/authMiddleware");
 
-router.get("", categoriesController.getAllCategories);
-router.get("/:id", categoriesController.getCategoryById);
-router.post("", categoriesController.createCategory);
-router.put("/:id", categoriesController.updateCategoryById);
-router.delete("/:id", categoriesController.deleteCategoryById);
+router.get("", validateToken, isAdmin, categoriesController.getAllCategories);
+router.get(
+  "/:id",
+  validateToken,
+  isAdmin,
+  categoriesController.getCategoryById
+);
+router.post("", validateToken, isAdmin, categoriesController.createCategory);
+router.put(
+  "/:id",
+  validateToken,
+  isAdmin,
+  categoriesController.updateCategoryById
+);
+router.delete(
+  "/:id",
+  validateToken,
+  isAdmin,
+  categoriesController.deleteCategoryById
+);
 
 module.exports = router;
