bbf1b99aa7d8bce78f4cfcd942013a16957bbfcb
gonzacs96 <gonzalosalinasss.96@gmail.com>
2022-04-25 16:43:00 -0300

    add route patch (update user)

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const updateUser=require("../controllers/updateUserController")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.patch("/:id",updateUser)
 
 module.exports = router;
