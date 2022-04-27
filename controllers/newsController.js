049d35abf76680ccef51e5b262ccb9863c9d13b7
Paolo Belforte <75280184+belfortep@users.noreply.github.com>
2022-04-27 09:52:38 -0300

    updated deleteNewById controller
    changed method name, first search "new" in database, then delete it

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,24 +1,27 @@
+const { News } = require("../models");
 const { HttpCodesEnum } = require('../enums/httpCodesEnum');
-const { News } = require('../models/index');
 
+exports.getAllNews = async (req, res, next) => { };
+exports.getNewById = async (req, res, next) => { };
+exports.createNew = async (req, res, next) => { };
+exports.updateNewById = async (req, res, next) => { };
+exports.deleteNewById = async (req, res, next) => {
+   try {
 
-const deleteNewsById = async (req, res) => {
-    try {
+        const deletingNew = await News.findOne({ where: { id: req.params.id } });   //I had to name it "deletingNew" because only "new" is a reserved word
 
-        const deletedNews = await News.destroy({ where: { id: req.params.id } });
-
-        if (!deletedNews) {
-            return res.status(HttpCodesEnum.NOT_FOUND).send('News not found');
+        if (!deletingNew) {
+            return res.status(HttpCodesEnum.NOT_FOUND).send('New not found');
         };
+
+        await deletingNew.destroy();
 
         return res.sendStatus(HttpCodesEnum.DELETED)
 
     } catch (err) {
         return res.status(HttpCodesEnum.SERVER_INTERNAL_ERROR).json({message: err.message});
-    }
+    }  
+};
 
-}
 
-module.exports = {
-    deleteNewsById
-}
+
