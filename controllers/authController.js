5a01d8706cbc0509b756250e8b88e9e89a78d942
valeushu <valeushuaia@hotmail.com>
2022-05-02 10:29:25 -0300

    feature/OT174-37

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,6 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const { User } = require("../models/index");
 const { HttpCodesEnum } = require("../enums/httpCodesEnum");
 
 exports.me = (req, res) => {
-  res.status(200).json({ status: "me", data: req.user });
+  res.status(HttpCodesEnum.OK).json({ status: "me", data: req.user });
 };
 
