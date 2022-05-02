61e0533676cee9bd6a611370f73b3832548bd746
valeushu <valeushuaia@hotmail.com>
2022-05-02 10:18:54 -0300

    feature/OT174-37

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,42 +1,17 @@
-const jwt = require('jsonwebtoken');
-//const  where  = require('sequelize/types');
-const { User } = require("../models/index");
+const jwt = require("jsonwebtoken");
 
- const authenticate = async (req, res, next) => {
-    try {
-      let token = req.header("Authorization");
-     // console.log(token)
-      if (!token) return res.status(403).json({ status: "no token provided" });
-      token = token.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.SECRET_JWT_KEY);
-      console.log(decoded)
-      req.userId = decoded.id;
-      console.log( req)
-   
-      const user = await User.findOne({where : {req}});
-      console.log(user.id);
-      req.dataUser = user;
-      if (!user) return res.status(404).json({ status: "no user found" });
-      req.token = token
-      next();
-    } catch (error) {
-      console.log(error)
-      return res.status(401).json({ status: "unauthorized" });
-    }
-  };
-  
-
-/*
 const authenticate = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
 
-    const token = req.headers.authorization?.split(' ')[1];
+  const verified = jwt.verify(token, process.env.SECRET_JWT_KEY);
 
-    const verified = jwt.verify(token, process.env.SECRET_JWT_KEY);
+  req.token = verified.user;
 
-    req.token = verified.user;
+  req.user = verified;
 
-    next();
-    
-}*/
+  console.log(verified);
 
-module.exports = {authenticate}
+  next();
+};
+
+module.exports = { authenticate };
