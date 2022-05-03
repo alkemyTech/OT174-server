ebbe0c3959935c7254302fbc56c633320c95add1
valeushu <84677463+valeushu@users.noreply.github.com>
2022-05-03 14:28:24 -0300

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const validateUser = require("../validators/validateUser");
-const registerController = require("../controllers/loginController");
+const registerController = require("../controllers/registerController");
 
 router.post("/register", validateUser, registerController);
 
